2148bad87ec5043ea40db80f9baf7e912f6c7abf
Ryan Gaige <Ryan@fencepencil.com>
2018-07-19 21:10:48 -0600

    added progess bar and health counter

--- game.js
+++ game.js
@@ -1,10 +1,45 @@
 var health = 100;
+var hits = -1;
 
-function slap(slap) {
-  var slap = health;
+function slap() {
   health -= 1;
-
-  console.log(health);
+  update();
 }
 
-slap();
+function punch() {
+  health -= 5;
+  update();
+}
+
+function kick() {
+  health -= 10;
+  update();
+}
+
+function increaseHits() {
+  hits++;
+}
+
+function update() {
+  increaseHits();
+  document.getElementById("health").innerText = health;
+  document.getElementById("hits").innerText = hits;
+  document.getElementById("progress").style.width = health + "%";
+
+  if (health <= 0) {
+    document.getElementById("progress").style.width = 0;
+    document.getElementById("health").innerText = 0;
+  }
+
+  // if (health == 0) {
+  //   alert: "You die";
+  // } else {
+  // }
+
+  // console.log(health);
+
+  // hits++;
+  // console.log(hits);
+}
+
+update();
