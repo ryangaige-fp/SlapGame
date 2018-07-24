be4c0f12c9ae4c85db0f48ffd8df28e874c947d9
Ryan Gaige <Ryan@fencepencil.com>
2018-07-23 21:00:32 -0600

    added health regeneration

--- game.js
+++ game.js
@@ -1,28 +1,63 @@
-var health = 100;
-var hits = -1;
+// let health = 100;
+let hits = -1;
+
+let paleGuy = {
+  name: "Pale Guy",
+  health: 100,
+  items: []
+};
 
 let Obj = function(name, modifier, description) {
   this.name = name;
   this.modifier = modifier;
   this.description = description;
+  items: [];
 };
 
 let items = {
-  poison: new Item("Poison", 9, "Slowly kills you from the inside out!")
+  mushroom: new Item(
+    "Magic shroom 'Brah'",
+    16,
+    "eat these and your health will grow grow grow!"
+  ),
+  soul: new Item("Soul Drain", 50, "Sucks out your soul and regerates life!"),
+  lifePotion: new Item(
+    "Life Potion",
+    26,
+    "Given to you from the old lady in the cave, restores your health!"
+  )
 };
 
+function giveMushroom() {
+  paleGuy.items.push("mushroom");
+  paleGuy.health += 16;
+  update();
+}
+
+function giveSoul() {
+  paleGuy.items.push("soul");
+  paleGuy.health += 50;
+  update();
+}
+
+function giveLifePotion() {
+  paleGuy.items.push("lifePotion");
+  paleGuy.health += 26;
+  update();
+}
+
 function slap() {
-  health -= 1;
+  paleGuy.health -= 1;
   update();
 }
 
 function punch() {
-  health -= 5;
+  paleGuy.health -= 5;
   update();
 }
 
 function kick() {
-  health -= 10;
+  paleGuy.health -= 10;
   update();
 }
 
@@ -30,17 +65,24 @@ function increaseHits() {
   hits++;
 }
 
+update();
+
 function update() {
   increaseHits();
-  document.getElementById("health").innerText = health;
+  document.getElementById("health").innerText = paleGuy.health;
   document.getElementById("hits").innerText = hits;
-  document.getElementById("progress").style.width = health + "%";
+  document.getElementById("progress").style.width = paleGuy.health + "%";
+  document.getElementById("EnemyName").innerText = paleGuy.name;
 
-  if (health <= 0) {
+  if (paleGuy.health <= 0) {
     document.getElementById("progress").style.width = 0;
     document.getElementById("health").innerText = 0;
   }
 
+  if (paleGuy.health >= 100) {
+    document.getElementById("progress").style.width = 100;
+    document.getElementById("health").innerText = 100;
+  }
   // if (health == 0) {
   //   alert: "You die";
   // } else {
@@ -51,5 +93,3 @@ function update() {
   // hits++;
   // console.log(hits);
 }
-
-update();
