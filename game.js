0a5e3a250d53ed5de9f9082b9bd44d849ade8120
Ryan Gaige <Ryan@fencepencil.com>
2018-07-24 20:58:53 -0600

    finished hits and health up

--- game.js
+++ game.js
@@ -7,11 +7,11 @@ let char1 = {
   items: []
 };
 
-let Character = function(name, health, items[]){
+let Character = function(name, health) {
   this.name = name;
   this.health = health;
-  this.item = items[];
-}
+  items: [];
+};
 
 let Obj = function(name, modifier, description) {
   this.name = name;
@@ -21,15 +21,15 @@ let Obj = function(name, modifier, description) {
 };
 
 let items = {
-  mushroom: new Items(
+  mushroom: new Obj(
     "Magic shroom 'Brah'",
     5,
     "eat these and your health will grow grow grow!"
   ),
-  soul: new Items("Soul Drain", 15, "Sucks out your soul and regerates life!"),
-  lifePotion: new Items(
+  soul: new Obj("Soul Drain", 40, "Sucks out your soul and regenerates life!"),
+  lifePotion: new Obj(
     "Life Potion",
-    30,
+    15,
     "Given to you from the old lady in the cave, restores your health!"
   )
 };
@@ -37,55 +37,61 @@ let items = {
 function giveMushroom() {
   char1.items.push("mushroom");
   char1.health += 5;
+
   update();
 }
 
 function giveSoul() {
   char1.items.push("soul");
-  char1.health += 15;
+  char1.health += 40;
   update();
 }
 
 function giveLifePotion() {
   char1.items.push("lifePotion");
-  char1.health += 30;
+  char1.health += 15;
   update();
 }
 
 function slap() {
-  if (
-    document.getElementById("monsterImage").src == "Assets/Photos/Dhalsim.gif"
-  ) {
-    document.getElementById("monsterImage").src = "Assets/Photos/Dhalsim.gif";
-  } else {
-    document.getElementById("monsterImage").src = "Assets/Photos/punching.gif";
-  }
+  // if (
+  //   document.getElementById("monsterImage").src == "Assets/Photos/Dhalsim.gif"
+  // ) {
+  //   document.getElementById("monsterImage").src = "Assets/Photos/Dhalsim.gif";
+  // } else {
+  //   document.getElementById("monsterImage").src = "Assets/Photos/punching.gif";
+  // }
   char1.health -= 1;
+  increaseHits();
   update();
 }
 
 function punch() {
-  if (
-    document.getElementById("monsterImage").src == "Assets/Photos/Dhalsim.gif"
-  ) {
-    document.getElementById("monsterImage").src = "Assets/Photos/Dhalsim.gif";
-  } else {
-    document.getElementById("monsterImage").src = "Assets/Photos/dhalPunch.gif";
-  }
+  // if (
+  //   document.getElementById("monsterImage").src == "Assets/Photos/Dhalsim.gif"
+  // ) {
+  //   document.getElementById("monsterImage").src = "Assets/Photos/Dhalsim.gif";
+  // } else {
+  //   document.getElementById("monsterImage").src = "Assets/Photos/dhalPunch.gif";
+  // }
+
   char1.health -= 5;
+  increaseHits();
   update();
 }
 
 function kick() {
-  if (
-    document.getElementById("monsterImage").src == "Assets/Photos/Dhalsim.gif"
-  ) {
-    document.getElementById("monsterImage").src = "Assets/Photos/Dhalsim.gif";
-  } else {
-    document.getElementById("monsterImage").src =
-      "Assets/Photos/Dhalsim-SFA2-Heavy-Air-Kick.gif";
-  }
+  // if (
+  //   document.getElementById("monsterImage").src == "Assets/Photos/Dhalsim.gif"
+  // ) {
+  //   document.getElementById("monsterImage").src = "Assets/Photos/Dhalsim.gif";
+  // } else {
+  //   document.getElementById("monsterImage").src =
+  //     "Assets/Photos/Dhalsim-SFA2-Heavy-Air-Kick.gif";
+  // }
+
   char1.health -= 10;
+  increaseHits();
   update();
 }
 
@@ -93,26 +99,28 @@ function increaseHits() {
   hits++;
 }
 
-update();
-
 function update() {
-  increaseHits();
   document.getElementById("health").innerText = char1.health;
   document.getElementById("hits").innerText = hits;
   document.getElementById("progress").style.width = char1.health + "%";
   document.getElementById("EnemyName").innerText = char1.name;
 
   if (char1.health <= 0) {
+    char1.health = 0;
     document.getElementById("progress").style.width = 0;
     document.getElementById("health").innerText = 0;
   }
 
   if (char1.health >= 100) {
+    char1.health = 100;
     document.getElementById("progress").style.width = 100;
     document.getElementById("health").innerText = 100;
   }
+
   // if (health == 0) {
   //   alert: "You die";
   // } else {
   // }
 }
+
+update();
